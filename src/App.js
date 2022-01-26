13731679b11af4902739b4dc9e3caf647932bb65
Murat ATALIK <62357472+murat-atalik@users.noreply.github.com>
2022-01-26 11:40:03 +0300

    Footer Component completed.
    Footer component works fine and responsive and also animation works well. I was looking for a better way to import external script and used a custom hook for it.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './style/index.scss';
 
+import { useState } from 'react';
+
+import Footer from './components/Footer/Footer';
+import useScript from './hooks/useScript';
+
 function App() {
+  const [openFooter, setOpenFooter] = useState(false);
+
+  useScript(
+    'https://holonext.azurewebsites.net/public/js/hn-sdk-embed-v4-without-variants.js'
+  );
+  useScript('https://kit.fontawesome.com/f5200b16a7.js');
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Footer openFooter={openFooter} setOpenFooter={setOpenFooter} />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,65 @@
+import './footer.scss';
+
+import React from 'react';
+import { useState } from 'react/cjs/react.development';
+
+import logo from '../../assets/images/holonext-logo.png';
+
+function Footer({ openFooter, setOpenFooter }) {
+  const [containerClass, setContainerClass] = useState('footer');
+  const [imageContainer, setImageContainer] = useState('');
+  const handleFooterOpen = () => {
+    setContainerClass('footer-close');
+    setImageContainer('image-close');
+    setOpenFooter(true);
+  };
+  const handleFooterClose = () => {
+    setTimeout(() => {
+      setOpenFooter(false);
+    }, 480);
+    setContainerClass('footer-open');
+    setImageContainer('image-open');
+  };
+  return (
+    <footer className={containerClass}>
+      {!openFooter ? (
+        <button
+          onClick={handleFooterOpen}
+          type="button"
+          className="open-menu-button"
+        >
+          <i className="fas fa-bars " />
+        </button>
+      ) : (
+        <button
+          onClick={handleFooterClose}
+          type="button"
+          className="close-menu-button"
+        >
+          <i className="fas fa-times" />
+        </button>
+      )}
+      {openFooter && (
+        <div
+          className={
+            !openFooter
+              ? 'close-menu-container-hidden'
+              : `close-menu-container ${containerClass}`
+          }
+        >
+          <div className="logo">
+            <img src={logo} alt="holonext" className={imageContainer} />
+          </div>
+          <div className="footer-content">
+            <a href="https://www.holonext.com/">About Us</a>
+            <a href="https://www.holonext.com/">Company</a>
+            <a href="https://www.holonext.com/">Cookie Policy</a>
+            <a href="https://www.holonext.com/">Terms of Use</a>
+          </div>
+        </div>
+      )}
+    </footer>
+  );
+}
+
+export default Footer;
